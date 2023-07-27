57ed84deeb8fcbb39e61448e5f4b18c54de072bc
DaniloPV18 <damipive1809@gmail.com>
2023-07-27 18:23:16 -0500

    Services added in Application Layer

---
+++ HRLeaveManagment.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace HRLeaveManagment.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection _services)
+        {
+            _services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            _services.AddMediatR(Assembly.GetExecutingAssembly());
+            return _services;
+        }
+    }
+}
