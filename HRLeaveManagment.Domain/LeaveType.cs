58e0e18421cef4446ad11b115664a9ba237e082b
DaniloPV18 <damipive1809@gmail.com>
2023-07-27 18:14:53 -0500

    Domain Layer Added

---
+++ HRLeaveManagment.Domain/LeaveType.cs
@@ -0,0 +1,10 @@
+﻿using HRLeaveManagment.Domain.Common;
+
+namespace HRLeaveManagment.Domain
+{
+    public class LeaveType : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public int DefaultDays { get; set; }
+    }
+}
