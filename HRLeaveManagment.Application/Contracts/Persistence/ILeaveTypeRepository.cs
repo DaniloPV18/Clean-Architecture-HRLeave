3954d12f611e479f1eadfc84bd1f77a240a72d13
DaniloPV18 <damipive1809@gmail.com>
2023-08-05 12:52:50 -0500

    [ADDED] Boolean method to unique name in Interface

--- HRLeaveManagment.Application/Contracts/Persistence/ILeaveTypeRepository.cs
+++ HRLeaveManagment.Application/Contracts/Persistence/ILeaveTypeRepository.cs
@@ -4,6 +4,6 @@ namespace HRLeaveManagment.Application.Contracts.Persistence
 {
     public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
     {
-
+        Task<bool> isTypeLeaveNameUnique(string name);
     }
 }
