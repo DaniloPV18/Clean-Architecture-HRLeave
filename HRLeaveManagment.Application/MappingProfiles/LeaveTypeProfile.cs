aaf0b6db00d6f56508ce20799a3a81ae80f5f95e
DaniloPV18 <damipive1809@gmail.com>
2023-08-05 13:02:17 -0500

    [ADDED] LeaveTypeProfile class for mapping DTO classes

---
+++ HRLeaveManagment.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HRLeaveManagment.Application.Features.LeaveType.Queries.GetAllLeaveType;
+using HRLeaveManagment.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+using HRLeaveManagment.Domain;
+
+namespace HRLeaveManagment.Application.MappingProfiles
+{
+    public class LeaveTypeProfile : Profile
+    {
+        public LeaveTypeProfile()
+        {
+            CreateMap<LeaveTypeDTO, LeaveType>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDetailsDTO>();
+        }
+    }
+}
