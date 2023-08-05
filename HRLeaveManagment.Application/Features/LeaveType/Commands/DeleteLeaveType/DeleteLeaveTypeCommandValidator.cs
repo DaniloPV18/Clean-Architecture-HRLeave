8fa5d08e9c30b20152b74b8fa5dc688c0f43e6c5
DaniloPV18 <damipive1809@gmail.com>
2023-08-05 12:50:15 -0500

    [ADDED] DeleteLeaveType Validator with rules - FluentValidation

---
+++ HRLeaveManagment.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using HRLeaveManagment.Application.Contracts.Persistence;
+
+namespace HRLeaveManagment.Application.Features.LeaveType.Commands.DeleteLeaveType
+{
+    public class DeleteLeaveTypeCommandValidator : AbstractValidator<DeleteLeaveTypeCommand>
+    {
+        public DeleteLeaveTypeCommandValidator()
+        {
+            RuleFor(p => p.Id)
+                .NotEmpty().WithMessage("{PropertyName} just numbers.");
+        }
+    }
+}
