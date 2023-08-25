00881a79bee46f169bb20b8984845a9a8bedc256
DaniloPV18 <damipive1809@gmail.com>
2023-08-25 15:56:14 -0500

    [EDIT] Added attribute

--- HRLeaveManagment.Domain/LeaveAllocation.cs
+++ HRLeaveManagment.Domain/LeaveAllocation.cs
@@ -8,5 +8,6 @@ namespace HRLeaveManagment.Domain
         public LeaveType? LeaveType { get; set; }
         public int LeaveTypeId { get; set; }
         public int Period { get; set; }
+        public string EmployeeId { get; set; } = string.Empty;
     }
 }
