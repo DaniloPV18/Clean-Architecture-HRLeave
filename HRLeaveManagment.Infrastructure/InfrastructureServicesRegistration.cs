92d0411e11e342e7183ef37d095eb94eba98a7b9
DaniloPV18 <damipive1809@gmail.com>
2023-08-25 17:43:14 -0500

    [ADDED] Implementations from layer Application/Contracts

---
+++ HRLeaveManagment.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,17 @@
+﻿using HRLeaveManagment.Application.Models;
+using HRLeaveManagment.Infrastructure.EmailService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HRLeaveManagment.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<EmailSender, EmailSender>();
+            return services;
+        }
+    }
+}
